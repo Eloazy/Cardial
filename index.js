a56cc0b31c53b15adc40231baf5d6fbfc8243a0e
Yume <149901413+Eloazy@users.noreply.github.com>
2025-02-16 00:20:20 -0300

    v1

---
+++ index.js
@@ -0,0 +1,31 @@
+import { Client, GatewayIntentBits } from 'discord.js'
+import pushBabel from './pushBabel.js'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new Client({intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent
+	]
+})
+
+client.on("ready", () => {
+	console.log("Cardial started")
+	console.log("by Babel systems")
+	console.log("---------------------")
+})
+
+client.on("interactionCreate", async (interaction) => {
+	const client_data = client
+	if(
+		interaction.isCommand() && 
+		interaction.user.bot == false &&
+		interaction.commandName == "rpg"
+	) {
+		pushBabel(interaction, interaction.options.getString('query'))
+	}
+})
+
+client.login(process.env.token)
---
+++ pushBabel.js
@@ -0,0 +1,19 @@
+export default async function(interaction, query) {
+    //return interaction.reply('unavaliable | incompatible with BabelServer 2.0')
+    await interaction.deferReply()
+    try {
+        const response = await fetch('http://localhost:1025/api/rpg', {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify({ query })
+        })
+        if(!response.ok) {throw new Error(`HTTP error! status: ${response.status}`)}
+        
+        const data = await response.json()
+        if(!data) {throw new Error(`Error: undefined data`)}
+        await interaction.editReply(data.result.result || data.result)
+    } catch(err) {interaction.editReply(`discord extension error: ${err}`)}
+}
+
